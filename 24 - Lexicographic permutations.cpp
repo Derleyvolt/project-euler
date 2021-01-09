9b7e460a2c92fc8244bcce736977b9124cb4d4f7
Wanderley <100Palavr@gmail.com>
2021-01-09 05:58:02 -0300

    Update 24 - Lexicographic permutations.cpp

--- 24 - Lexicographic permutations.cpp
+++ 24 - Lexicographic permutations.cpp
@@ -2,9 +2,7 @@
 
 using namespace std;
 
-#define vi vector<int>
-
-vi arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+vector<int> arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 void solve() {
     int cnt = 1;
