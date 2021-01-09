d4e7bd480252836c8e53a79b42e629ad0b903ce9
Wanderley <100Palavr@gmail.com>
2021-01-09 05:57:46 -0300

    Update 24 - Lexicographic permutations.cpp

--- 24 - Lexicographic permutations.cpp
+++ 24 - Lexicographic permutations.cpp
@@ -9,7 +9,7 @@ vi arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 void solve() {
     int cnt = 1;
     while (cnt < int(1e6)) {
-        next_permutation(arr.begin(), arr.end());
+        next_permutation(arr.begin(), arr.end()); // gera as permutações em ordem lexicográfica.
         cnt++;
     }   
     for (auto a : arr) cout << a;
