f078e42257edeccfd65c2cadba637a0418465513
Wanderley <100Palavr@gmail.com>
2021-01-12 12:33:56 -0300

    Create 5 - Smallest multiple.cpp

---
+++ 5 - Smallest multiple.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+int main() {
+    // basta ver a fatoração em primos..
+    int ans = pow(2, 4) * pow(3, 2) * 5 * 7 * 11 * 13 * 17 * 19;
+    cout << ans << endl;
+    return 0;
+}
