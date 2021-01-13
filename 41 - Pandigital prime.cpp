6cd0e3b85f87f40266f72b8af600348e13b115ae
Wanderley <100Palavr@gmail.com>
2021-01-13 17:08:12 -0300

    Update 41 - Pandigital prime.cpp

--- 41 - Pandigital prime.cpp
+++ 41 - Pandigital prime.cpp
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
 
-using namespace std; 
+using namespace std;
 
 #define ll long long
 
-// É ainda possível fazer várias podas nesse código e no próprio problema..
+// o resultado deve ser alguma permutação desses números..
 
-vector<int> num = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-
-ll ans;
+vector<string> permut = {
+                            {"1"},
+                            {"21"},
+                            {"321"},
+                            {"4321"},
+                            {"54321"},
+                            {"654321"},
+                            {"7654321"},
+                            {"87654321"},
+                            {"987654321"}
+                        };
 
 bool is_prime(ll n) {
     if(n < 2)
         return false;
     if(n == 2)
-        return true; 
+        return true;
     for(int i = 2; i * i <= n; i++) {
         if(n % i == 0)
             return false;
@@ -22,39 +30,20 @@ bool is_prime(ll n) {
     return true;
 }
 
-ll biggest_prime(string& s) {
-    for(auto a:s)
-        if((a-'0') > s.length())
-            return 0;
-    
-    ll ans = 0;
-    while(not ans) {
-        ll n = stoll(s);
-        if(is_prime(n))
-            ans = max(ans, n);
-        if(::ans > n or not prev_permutation(s.begin(), s.end()))
-            break;
-    }
-    return ans;
-}
-
-void solve() {
-    int maxn = (1<<9) - 1;
-    for(int i = maxn; i > 0 ; i--) {
-        string str;
-        for(int j = 0; j < 9; j++) {
-            if(i & (1 << j))
-                // o número de n dígitos obtido aqui é o maior, lexicograficamente,
-                // então eu uso apenas prev_permutation.. portanto, o primeiro
-                // primo que eu encontrar será o maior possível naquele subconjunto.
-                str.push_back('0' + num[j]);
+ll solve() {
+    reverse(permut.begin(), permut.end());
+    for(auto num:permut) {
+        for(;;) {
+            if(is_prime(stoi(num)))
+                return stoi(num);
+            // gera uma sequência lexicográfica decrescente..
+            if(not prev_permutation(num.begin(), num.end()))
+                break;
         }
-        ans = max(ans, biggest_prime(str));
     }
-    cout << ans << endl;
+    return int("O CÓDIGO NUNCA CHEGARÁ AQUI..");
 }
 
 int main() {
-    solve();
-    return 0;
+    cout << solve() << endl;
 }
